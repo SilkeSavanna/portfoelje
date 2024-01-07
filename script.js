0bac2b95992dc97717b56d14b0a5ec274a70d21a
SilkeSavanna <sihe0002@stud.kea.dk>
2024-01-07 18:45:37 +0100

    spilside_forside

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const galleriTitel = document.querySelector(".galleri_title");
+const imgGalleri = document.querySelector(".billede_galleri");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+galleriTitel.addEventListener("mouseover", function () {
+  imgGalleri.style.display = "block";
+});
+
+galleriTitel.addEventListener("mouseout", function () {
+  imgGalleri.style.display = "none";
 });
