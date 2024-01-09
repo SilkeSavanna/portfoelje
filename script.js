c269f003d88538cca56ad2f26f35071ff04a6a80
SilkeSavanna <sihe0002@stud.kea.dk>
2024-01-09 11:20:35 +0100

    javascript_og_forside

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const galleriTitel = document.querySelector(".galleri_title");
-const imgGalleri = document.querySelector(".billede_galleri");
+const Projekter1 = document.querySelector(".projekter_1");
+const textContainer = document.getElementById("textContainer");
+const hoverImage = document.getElementById("hoverImage");
+
+/*burgermenu*/
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
-galleriTitel.addEventListener("mouseover", function () {
-  imgGalleri.style.display = "block";
-});
+Projekter1.classList.add("puls_img");
 
-galleriTitel.addEventListener("mouseout", function () {
-  imgGalleri.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+  const textContainer = document.getElementById("textContainer");
+  const hoverImage = document.getElementById("hoverImage");
+
+  if (textContainer && hoverImage) {
+    // Tjek om elementerne findes, før du tilføjer event listeners
+    textContainer.addEventListener("mouseover", () => {
+      hoverImage.style.display = "block";
+    });
+
+    textContainer.addEventListener("mouseout", () => {
+      hoverImage.style.display = "none";
+    });
+  }
 });
